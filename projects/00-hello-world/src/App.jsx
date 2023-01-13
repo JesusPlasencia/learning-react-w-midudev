1547013d0a6225835f67258dff059c4dfacf8e64
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2023-01-13 17:04:36 -0500

    00-hello-world--commit-01

---
+++ projects/00-hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+export function App() {
+    return (
+        <article style={{ display: 'flex', color: "#fff" }}>
+            <header>
+                <img alt="El avatar de midudev" src="https://unavatar.io/midudev" />
+                <div>
+                    <strong>Miguel Angel Duran</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
