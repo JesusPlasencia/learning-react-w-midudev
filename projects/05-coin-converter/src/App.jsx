4d0ca4f98c719d2d5c72b6cc424b3e53e4f5b0ac
Jesus Plasencia Toledo <jesusplasencia2508@hotmail.com>
2023-02-15 12:08:05 -0500

    feat: project title

--- projects/05-coin-converter/src/App.jsx
+++ projects/05-coin-converter/src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import './../node_modules/spin.js/spin.css'
+
+// ! Components //
+
+// ! Components //
 
 function App () {
   return (
-    <div className='App'>
-      <h1>Hola</h1>
+    <div className='page'>
+
+      <header>
+        <h1 className='title'>X-Coin</h1>
+      </header>
+
+      <main id='spin' />
+
     </div>
   )
 }
